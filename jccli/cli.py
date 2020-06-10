96fad940866fdca728be8b08004c38ef339ddaa0
Connor Boyle <connor.boyle@sagebase.org>
2020-06-09 23:39:45 -0700

    Add New create-user Command
    It is based on option-specified parameters instead of JSON file/stdin. The command overall works, although all of the functionality has not yet been tested, and no real unit tests have been written for it.

--- jccli/cli.py
+++ jccli/cli.py
@@ -41,25 +41,38 @@ def cli(ctx, key):
     }
 
 @cli.command()
-@click.option('--json', "-j", required=False, type=str,
-              help='SystemUser properties json')
-@click.option('--file', "-f", required=False, type=click.Path(exists=True),
-              help='SystemUser properties file')
+@click.option('--username', '-u', required=True, type=str)
+@click.option('--email', '-e', required=True, type=str)
+@click.option('--first-name', '-f', type=str, default='')
+@click.option('--last-name', '-l', type=str, default='')
+@click.option('--account-locked', is_flag=True)
+@click.option('--activated', is_flag=True)
+@click.option('--allow-public-key/--disallow-public-key', default=True)
+@click.option('--ldap-binding-user', is_flag=True)
+@click.option('--passwordless-sudo', is_flag=True)
+@click.option('--sudo', is_flag=True)
 @click.pass_context
-def create_user(ctx, json, file):
+def create_user(ctx, username, email, first_name, last_name, account_locked, activated, allow_public_key,
+                ldap_binding_user, passwordless_sudo, sudo):
     """
     Create a new Jumpcloud user
     """
+    # TODO: Test whether the flags are working. So far, --disallow-public-key does work, but --activated doesn't seem to
+    #  be doing anything!
     api1 = JumpcloudApiV1(ctx.obj.get('key'))
-    user = {}
-    if json is not None:
-        user = jccli_helpers.get_user_from_term(json)
-    elif file is not None:
-        user = jccli_helpers.get_user_from_file(file)
-    else:
-        raise MissingRequiredArgumentError("SystemUser properties not provided")
-
-    response = api1.create_user(user)
+    systemuser = {
+        'username': username,
+        'email': email,
+        'first_name': first_name,
+        'last_name': last_name,
+        'account_locked': str(account_locked),
+        'activated': str(activated),
+        'allow_public_key': str(allow_public_key),
+        'ldap_binding_user': str(ldap_binding_user),
+        'passwordless_sudo': str(passwordless_sudo),
+        'sudo': str(sudo)
+    }
+    response = api1.create_user(systemuser)
     LOGGER.info(f"{response}")
 
 # @cli.command()
