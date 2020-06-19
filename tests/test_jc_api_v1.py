1ccb14596b0fab55cbf4f324af222b5b4a66b0f5
Connor Boyle <connor.boyle@sagebase.org>
2020-06-19 15:31:12 -0700

    Change get_user() to Use Only One API Call
    Instead of first making a list call to fish for a user ID, then using that user ID for a list

--- tests/test_jc_api_v1.py
+++ tests/test_jc_api_v1.py
@@ -85,74 +85,37 @@ class TestJcApiV1:
     @patch.object(JumpcloudApiV1,'get_users')
     def test_get_user_id_not_found(self, mock_get_users):
         response = [
-            ObjectView({
-                'account_locked': False,
-                'activated': False,
-                'addresses': [],
-                'allow_public_key': True,
-                'attributes': [],
-                'bad_login_attempts': 0,
-                'company': None,
-                'cost_center': None,
-                'created': '2019-11-05T01:42:36.652Z',
-                'department': None,
-                'description': None,
-                'displayname': None,
+            Systemuserreturn({
                 'email': 'jc.tester1@sagebase.org',
-                'employee_identifier': None,
-                'employee_type': None,
-                'enable_managed_uid': False,
-                'enable_user_portal_multifactor': False,
-                'external_dn': None,
-                'external_source_type': None,
-                'externally_managed': False,
                 'firstname': 'JC',
-                'id': '5dc0d38c1e2e5f51f2312948',
-                'job_title': None,
                 'lastname': 'Tester1',
-                'ldap_binding_user': False,
-                'location': None,
-                'mfa': {'configured': False, 'exclusion': False, 'exclusion_until': None},
-                'middlename': None,
-                'organization': '5a9d7329feb7f81004ecbee4',
-                'password_expiration_date': None,
-                'password_expired': False,
-                'password_never_expires': False,
-                'passwordless_sudo': False,
-                'phone_numbers': [],
-                'public_key': None,
-                'relationships': [],
-                'samba_service_user': False,
-                'ssh_keys': [],
-                'sudo': False,
-                'tags': None,
-                'totp_enabled': False,
-                'unix_guid': 5109,
-                'unix_uid': 5109,
-                'username': 'jctester1',
+                'username': 'jctester1'
             })
         ]
         api1 = JumpcloudApiV1("1234")
         mock_get_users.return_value = response
-        user_id = api1.get_user_id("foo")
-        assert (
-             user_id == None
-        ), "User ID should be none"
+        with pytest.raises(SystemUserNotFoundError):
+            api1.get_user_id("foo")
 
-    @patch.object(jcapiv1.SystemusersApi, 'systemusers_get')
-    @patch.object(JumpcloudApiV1, 'get_user_id')
-    def test_get_user(self, mock_get_user_id, mock_systemusers_get):
-        user = {
-            'username': 'fake_user',
-            'id': '1234',
-            'firstname': 'Steve'
-        }
-        mock_systemusers_get.return_value = Systemuserreturn(**user)
-        mock_get_user_id.return_value = '1234'
+    @patch.object(jcapiv1.SystemusersApi, 'systemusers_list')
+    def test_get_user(self, mock_systemusers_list):
+        users = [
+            Systemuserreturn(**{
+                'username': 'fake_user',
+                'id': '1234',
+                'firstname': 'Steve'
+            }),
+            Systemuserreturn(**{
+                'username': 'fake_user_two',
+                'id': '4321',
+                'firstname': 'Angela'
+            })
+        ]
+        mock_systemusers_list.return_value = users
         api1 = JumpcloudApiV1("fake_key")
         firstname = api1.get_user(username='fake_user')['firstname']
         assert (
-            firstname == user['firstname']
+            firstname == 'Steve'
         ), "Failed to retrieve correct user object"
 
     @patch.object(jcapiv1.SystemusersApi, 'systemusers_put')
