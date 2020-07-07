9e1cf974b5dafb9b5de12e9f81da02c674015785
Khai Do <zaro0508@gmail.com>
2020-07-07 10:22:49 -0700

    bump version
    Update version to set travis ci up to automatically publish
    and bump version on the next release.

--- jccli/version.py
+++ jccli/version.py
@@ -8,5 +8,5 @@
 This module contains project version information.
 """
 
-__version__ = "0.0.8"  #: the working version
-__release__ = "0.0.8"  #: the release version
+__version__ = "0.0.9"  #: the working version
+__release__ = "0.0.9"  #: the release version
