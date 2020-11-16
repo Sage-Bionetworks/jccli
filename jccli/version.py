1de9a7f493d14bbbe6f3d201eb1aa989cdeec5bb
Travis CI User <travis@example.org>
2020-11-16 19:44:32 +0000

    [ci skip] bump version

--- jccli/version.py
+++ jccli/version.py
@@ -8,5 +8,5 @@
 This module contains project version information.
 """
 
-__version__ = "0.0.17"  #: the working version
-__release__ = "0.0.17"  #: the release version
+__version__ = "0.0.18"  #: the working version
+__release__ = "0.0.18"  #: the release version
